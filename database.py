054663c99ce12225c1091a1a9dadb92de4f05db6
Jose Carlos <josealarconchacon@gmail.com>
2021-07-27 13:07:14 -0400

    implement function to add and fetch movies

--- database.py
+++ database.py
@@ -1,3 +1,8 @@
+import datetime
+from os import pathconf_names
+import sqlite3
+from sqlite3.dbapi2 import Cursor
+
 # create table query
 CREATE_Movies_table = """CREATE TABLE IF NOT EXISTS movies (
     title TEXT,
@@ -11,3 +16,33 @@ SELECT_ALL_MOVIES = "SELECT * FROM movies;"
 # select the upcoming movies
 SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > ?;"
 SELECT_WATCHED_MOVIES = "SELECT * FROM movies WHERE watched = 1;"
+
+connection = sqlite3.connect("myData.db")
+
+def create_tables():
+    with connection:
+        connection.execute(CREATE_Movies_table)
+
+def add_movie(title, release_timestamp):
+    with connection:
+        connection.execute(INSERT_MOVIES, (title, release_timestamp))
+
+def get_movies(upcoming=False):
+    with connection:
+        cursor = connection.cursor()
+        # if upcoming = True
+        if upcoming:
+            today_timestamp = datetime.datetime.today().timestamp()
+            cursor.execute(SELECT_UPCOMING_MOVIES, (today_timestamp,))
+        else:
+            cursor.execute(SELECT_ALL_MOVIES)
+        return cursor.fetchall()
+
+def watch_movie(title):
+    pass
+
+def get_watched_movies():
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(SELECT_WATCHED_MOVIES)
+        return cursor.fetchall()
