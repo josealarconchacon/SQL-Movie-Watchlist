52375338eb7239950a54e60e8a7351bc3c409f44
Jose Carlos <josealarconchacon@gmail.com>
2021-07-27 22:45:26 -0400

    add create new table to execute

--- database.py
+++ database.py
@@ -34,6 +34,7 @@ connection = sqlite3.connect("myMovieData.db")
 def create_tables():
     with connection:
         connection.execute(CREATE_MOVIES_TABLE)
+        connection.execute(CREATE_WATCHLIST_TABLE)
 
 def add_movie(title, release_timestamp):
     with connection:
